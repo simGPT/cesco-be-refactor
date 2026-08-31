02ee282628e9ac0588e4525e57004d8dc349b183
simGPT <simGPT02@gmail.com>
2026-08-31 16:30:59 +0900

    :recycle:Refactor: 미사용 UserDetailsService 및 Spring Cloud Config 설정 제거

--- src/main/java/com/practice/likelionhackathoncesco/global/config/SecurityConfig.java
+++ src/main/java/com/practice/likelionhackathoncesco/global/config/SecurityConfig.java
@@ -1,17 +1,12 @@
 package com.practice.likelionhackathoncesco.global.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,13 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
-  @Value("${spring.cloud.config.username}")
-  private String username;
 
-  @Value("${spring.cloud.config.password}")
-  private String password;
-
-  // CorsConfig에서 만든 CORS 설정을 주입받기
   @Autowired private UrlBasedCorsConfigurationSource corsConfigurationSource;
 
   @Bean
@@ -36,22 +25,8 @@ public class SecurityConfig implements WebMvcConfigurer {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.csrf(AbstractHttpConfigurer::disable)
-        .cors(cors -> cors.configurationSource(corsConfigurationSource)) // CORS 설정 추가
-        .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()) // 모든 요청 허용
-        // .httpBasic(Customizer.withDefaults())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource))
+        .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
         .build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService() {
-
-    UserDetails admin =
-        User.builder()
-            .username(username)
-            .password(passwordEncoder().encode(password))
-            .roles("ADMIN")
-            .build();
-
-    return new InMemoryUserDetailsManager(admin);
   }
 }
